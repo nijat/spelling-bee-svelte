fd04f5ac7d24f0b504a977f42462fe5470219d85
Nijat Rzayev <rzaev.nicat@gmail.com>
2022-07-26 17:58:18 +0400

    hex is fixed

---
+++ src/lib/hexagon/hex.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let center = true;
+	export let letter = '';
+
+	const topClass = center ? 'center-top' : 'hex-top';
+	const bottomClass = center ? 'center-bottom' : 'hex-bottom';
+	const midClassList = 'flex flex-col justify-center items-center';
+	const middleClass = center ? 'hex-center' : 'hex-middle';
+</script>
+
+<div class="hex my-16 rounded-full unselectable">
+	<div class={topClass} />
+	<div class="{midClassList} {middleClass}">
+		<h2 class="text-2xl font-bold">
+			{letter}
+		</h2>
+	</div>
+	<div class={bottomClass} />
+</div>
---
+++ src/lib/hexagon/hexagon.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Hex from '$lib/hexagon/hex.svelte';
+	export let listOfLetter = ['H', 'A', 'B', 'T', 'R', 'S'];
+	export let centerLetter = 'I';
+</script>
+
+<div class="max-w-md mx-auto">
+    <div class="w-full letters justify-center flex flex-row md:p-6">
+        <div class="left flex flex-col h-full ">
+            <Hex center={false} letter={listOfLetter[0]} />
+            <Hex center={false} letter={listOfLetter[1]} />
+        </div>
+        <div class=" flex flex-col h-full -mt-10 md:-mt-14">
+            <Hex center={false} letter={listOfLetter[2]} />
+            <Hex center={true} letter={centerLetter} />
+            <Hex center={false} letter={listOfLetter[3]} />
+        </div>
+        <div class="flex flex-col">
+            <Hex center={false} letter={listOfLetter[4]} />
+            <Hex center={false} letter={listOfLetter[5]} />
+        </div>
+    </div>
+</div>
