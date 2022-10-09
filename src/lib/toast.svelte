4ae9d42c025fe14e468ce0b4ca368e5d8e2d302b
Nijat Rzayev <rzaev.nicat@gmail.com>
2022-10-09 21:42:47 +0200

    css moved to main file

--- src/lib/toast.svelte
+++ src/lib/toast.svelte
@@ -3,14 +3,3 @@
 </script>
 
 <SvelteToast />
-
-<style>
-	:root {
-		--toastContainerTop: auto;
-		--toastContainerRight: auto;
-        --toastBackground: #F56565;
-        --toastBarBackground: #C53030;
-		--toastContainerBottom: 8rem;
-		--toastContainerLeft: calc(50vw - 8rem);
-	}
-</style>
