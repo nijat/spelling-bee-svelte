1047e58f38afc9abfae237070dea521bb9b0b2da
Nijat Rzayev <rzaev.nicat@gmail.com>
2022-10-24 10:36:51 +0200

    fix splash screen

--- src/lib/splash_screen.svelte
+++ src/lib/splash_screen.svelte
@@ -1,10 +1,7 @@
-<div class="flex items-center justify-center ">
-	<svg xmlns="http://www.w3.org/2000/svg" class="w-24 h-24 text-red-600 animate-spin" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-		<path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			stroke-width="1"
-			d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-		/>
-	</svg>
+<script>
+	import { Jumper } from 'svelte-loading-spinners';
+</script>
+
+<div class="grid h-screen place-items-center">
+	<Jumper size="60" color="#FF3E00" unit="px" duration="1s" />	
 </div>
